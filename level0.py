56b6c4507daa72a207e3dc6954c7d4c697910434
SharmilaJ14 <114463476+SharmilaJ14@users.noreply.github.com>
2024-01-06 10:37:06 +0530

    Update level0.py

--- level0.py
+++ level0.py
@@ -1,6 +1,59 @@
 #KLA Hackathon!
 #Level 0: Travelling salesman problem
+from typing import DefaultDict
 import json
+
+#Using Heuristics to solve it - Nearest Neighbour Heuristic
+"""def travellingSalesman(G):
+    visited=[False for i in range(len(G))]
+    visited[0]=True
+    currentVertex=0
+    while all(not i for i in visited):
+        minDist=min(G[currentVertex])
+        ind=[i for i,j in enumerate(G[currentVertex]) if j==minDist]
+        if(len(ind)==1):
+            if(visited[ind[0]]==False):
+                visited[currentVertex]=True
+                currentVertex=ind[0]
+        else:
+            for i in ind:
+                if(visited[i]==False):
+                    visited[currentVertex]=True
+                    currentVertex=i
+                    break"""
+
+def findMinRoute(tsp):
+    sum = 0
+    counter = 0
+    j = 0
+    i = 0
+    min = 100000000
+    visitedRouteList = DefaultDict(int)
+    visitedRouteList[0] = 1
+    route = [0] * len(tsp)
+    while i < len(tsp) and j < len(tsp[i]):
+        if counter >= len(tsp[i]) - 1:
+            break
+        if j != i and (visitedRouteList[j] == 0):
+            if tsp[i][j] < min:
+                min = tsp[i][j]
+                route[counter] = j + 1
+        j += 1
+        if j == len(tsp[i]):
+            sum += min
+            min = 100000000
+            visitedRouteList[route[counter] - 1] = 1
+            j = 0
+            i = route[counter] - 1
+            counter += 1
+    print(route)
+    i = route[counter - 1] - 1
+    for j in range(len(tsp)):
+        if (i != j) and tsp[i][j] < min:
+            min = tsp[i][j]
+            route[counter] = j + 1
+    sum += min
+    return sum,route[::-1]
 
 #Importing the data
 f0=open("C:/Users/TEMP/Desktop/Input data/level0.json")
@@ -23,3 +76,4 @@ for i in dGraph:
     k+=1
 
 #Fixing 'r0' vertex to be the first and last vertes and then solving travelling salesman problem
+print(findMinRoute(dMat))
