3f96db9fe5730981ba016e35e14e5ac77a6673c7
SharmilaJ14 <114463476+SharmilaJ14@users.noreply.github.com>
2024-01-06 14:38:22 +0530

    Update level1a.py

--- level1a.py
+++ level1a.py
@@ -6,5 +6,16 @@ f1=open("C:/Users/TEMP/Desktop/Input data/level1a.json")
 ip1=json.load(f1)
 print(ip1)
 
+n=ip1['n_neighbourhoods']
 qty=[]
-capacity=ip1['v']
+dMat=[[0 for i in range(n)]for j in range(n)]
+k=0
+for i in ip1['neighbourhoods']:
+    qty.append(ip1['neighbourhoods'][i]['order_quantity'])
+    for j in range(n):
+        dMat[k][j]=ip1['neighbourhoods'][i]['distances'][j]
+    k+=1
+
+restDist=ip1['restaurants']['r0']['neighbourhood_distance']
+
+capacity=ip1['vehicles']['v0']['capacity']
