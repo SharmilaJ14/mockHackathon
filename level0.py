e3c096b9aa1bcb85358acae6853177e3238fa1fd
SharmilaJ14 <114463476+SharmilaJ14@users.noreply.github.com>
2024-01-06 11:56:38 +0530

    Update level0.py

--- level0.py
+++ level0.py
@@ -1,27 +1,42 @@
-#KLA Hackathon!
+#Hackathon!
 #Level 0: Travelling salesman problem
 from typing import DefaultDict
 import json
 
 #Using Heuristics to solve it - Nearest Neighbour Heuristic
-"""def travellingSalesman(G):
+def travellingSalesman(G):
     visited=[False for i in range(len(G))]
-    visited[0]=True
     currentVertex=0
-    while all(not i for i in visited):
-        minDist=min(G[currentVertex])
-        ind=[i for i,j in enumerate(G[currentVertex]) if j==minDist]
-        if(len(ind)==1):
-            if(visited[ind[0]]==False):
-                visited[currentVertex]=True
-                currentVertex=ind[0]
-        else:
-            for i in ind:
-                if(visited[i]==False):
+    path=[0]
+    count=0
+    while any(not i for i in visited):
+        temp=G[currentVertex]
+        print(path,count,temp)
+        while(visited[currentVertex]==False):
+            temp.remove(min(temp))
+            minDist=min(temp)
+            ind=[i for i,j in enumerate(G[currentVertex]) if j==minDist]
+            if(len(ind)==1):
+                if(visited[ind[0]]==False):
                     visited[currentVertex]=True
-                    currentVertex=i
-                    break"""
+                    currentVertex=ind[0]
+                    path.append(currentVertex)
+                    count+=minDist
+                else:
+                    temp.pop(ind[0])
+            else:
+                for i in ind:
+                    if(visited[i]==False):
+                        visited[currentVertex]=True
+                        currentVertex=i
+                        path.append(currentVertex)
+                        count+=minDist
+                        break
+                    else:
+                        temp.pop(ind[0])
+    return path,count
 
+#Greedy Approach - finding the shortest path starting from vertex 0
 def findMinRoute(tsp):
     sum = 0
     counter = 0
@@ -30,6 +45,7 @@ def findMinRoute(tsp):
     min = 100000000
     visitedRouteList = DefaultDict(int)
     visitedRouteList[0] = 1
+    path=[]
     route = [0] * len(tsp)
     while i < len(tsp) and j < len(tsp[i]):
         if counter >= len(tsp[i]) - 1:
@@ -43,17 +59,47 @@ def findMinRoute(tsp):
             sum += min
             min = 100000000
             visitedRouteList[route[counter] - 1] = 1
+            path.append(route[counter]-1)
             j = 0
             i = route[counter] - 1
             counter += 1
-    print(route)
     i = route[counter - 1] - 1
     for j in range(len(tsp)):
         if (i != j) and tsp[i][j] < min:
             min = tsp[i][j]
             route[counter] = j + 1
     sum += min
-    return sum,route[::-1]
+    return sum,path
+
+#Using Nearest Neighbour Heuristic
+cost=0
+def tsp(c):
+    global cost
+    adj_vertex = 99999999
+    min_val = 99999999
+    visited[c] = 1
+    print((c + 1), end=" ")
+    for k in range(n):
+        if (dMat[c][k] != 0) and (visited[k] == 0):
+            if dMat[c][k] < min_val:
+                min_val = dMat[c][k]
+                adj_vertex = k
+    if min_val != 99999999:
+        cost = cost + min_val
+    if adj_vertex == 99999999:
+        adj_vertex = 0
+        print((adj_vertex + 1), end=" ")
+        cost = cost + dMat[c][adj_vertex]
+        return
+    tsp(adj_vertex)
+
+#Using Twice around the tree
+def findMST(G):
+    mst=[]
+    v=[i for i in range(len(G))]
+
+def TwiceAroundTree(G):
+    mst=findMST(G)
 
 #Importing the data
 f0=open("C:/Users/TEMP/Desktop/Input data/level0.json")
@@ -76,4 +122,17 @@ for i in dGraph:
     k+=1
 
 #Fixing 'r0' vertex to be the first and last vertes and then solving travelling salesman problem
-print(findMinRoute(dMat))
+cost,route=findMinRoute(dMat)
+visited=[0 for i in range(n+1)]
+#print(tsp(0), cost)
+op0={}
+path=[]
+for i in route:
+    path.append(f"n{i-1}")
+op0['v0']={"path":path}
+print(op0)
+
+#Output as file 
+f=open("C:/Users/TEMP/Desktop/Input data/level0_output.json",'w')
+json.dump(op0,f)
+
